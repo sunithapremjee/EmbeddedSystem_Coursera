3e7b5badada420ce508cd4b4ff4eb992bc93145a
Sunitha Premjee <sunitha.premjee@yahoo.com>
2018-04-11 13:31:31 -0600

    Creating initial Coursera Repository

---
+++ stats.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.h>
+ * @brief <Header file to analyze an array of unsigned char data items and report 
+analytics on the  maximum, minimum, mean,
+ * and median of the data set. In addition, data is reordered from large to small.
+ * All statistics are rounded down to the nearest integer.
+ * After analysis and sorting is done,  data is printed>
+ *
+ * @author <Sunitha Premjee>
+ * @date <04/08/2018>
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
